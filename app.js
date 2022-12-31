d2ea6d6d0a0e5e3919552d068863e586bc3aa0ef
Farouk adedamola <faroukadedamola1@gmail.com>
2022-12-31 23:45:57 +0100

    commit nav fix

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
 const menuBtn = document.querySelector(".menu-btn");
 backBtn = document.querySelector(".back-btn");
 menu = document.querySelector("nav");
+list = document.querySelector(".active");
 
 menuBtn.addEventListener("click", () => {
-  menu.style.transform = "translateX(0)";
+  if (window.innerWidth <= 870) {
+    menu.style.transform = "translateX(0)";
+  }
 });
 
 backBtn.addEventListener("click", () => {
   menu.style.transform = "translateX(-100%)";
+});
+
+list.addEventListener("click", () => {
+  list.style.background = "#2485ff";
 });
